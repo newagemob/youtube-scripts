83457b199bf52324aca5a337a36bcb753f9a3901
newagemob <isaiah.wright@tutanota.com>
2023-04-10 09:13:22 -0500

    changing to chatcompletion

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # youtube-scripts
+
+## TODO:
+
+* [ ] Automatically export scripts to a folder on local network or Google Drive
+* [ ] Train custom model based on ['VSauce', 'Michael Reeves', 'Stuff Made Here', 'Nile Red', 'William Osmond'] channels
--- articles_to_shorts.py
+++ articles_to_shorts.py
@@ -7,6 +7,7 @@ import time
 import datetime as dt
 from pathlib import Path
 import os
+import tqdm
 
 openai.api_key = os.environ.get('OPENAI_SECRET_KEY')
 
@@ -43,7 +44,7 @@ def get_tech_articles():
 
     # get the text and href attributes of the links
     articles = []
-    for link in links[0:10]:
+    for link in tqdm.tqdm(links[0:10]):
         article = {
             'title': link.text,
             'href': link['href'],
@@ -61,11 +62,12 @@ def get_tech_articles():
             continue
 
     # run the summaries
-    for article in articles:
+    for article in tqdm.tqdm(articles):
         article['paragraph'] = article['paragraph'][:4096]
         prompt = f"Please rewrite the following article as a complete one minute YouTube video. Keep the summary as concise as possible, but make it engaging and interesting, in the style of a VSauce video without ever referring to the narrator. If necessary, provide your own examples. Here is the title: {article['title']}. I'm feeding you the entire webpage's paragraph elements, so ignore any random or unrelated information: {article['paragraph']}"
 
-        generate_summary(title=article['title'], prompt=prompt, directory='tech_video_scripts')
+        generate_summary(
+            title=article['title'], prompt=prompt, directory='tech_video_scripts')
 
         time.sleep(2)
 
@@ -88,7 +90,8 @@ def get_programming_examples(language, method):
             soup = BeautifulSoup(response.text, "html.parser")
             paragraphs = soup.find_all('p')
             text = ' '.join([p.get_text() for p in paragraphs])
-            generate_summary(title=f"{language} {method}", prompt=text, directory='programming_video_scripts')
+            generate_summary(
+                title=f"{language} {method}", prompt=text, directory='programming_video_scripts')
         except:
             continue
 
@@ -127,11 +130,12 @@ def get_history_articles():
     posts = driver.find_elements(By.TAG_NAME, "h3")
     # only show the content inside the h3 tag
     posts = [post.text for post in posts]
-    
+
     for post in posts:
         if post != '':
             prompt = f"I'm going to give you a title of a post from a random history subreddit. Thoroughly research the post and write a complete and concise one minute YouTube video about the post. In the style of a VSauce video without ever referring to the narrator. Imagine you're explaining to someone who has never heard of it before, but never refer to the post as a post. If it's applicable, show the historical context and the cultural and social impact of the post. Here is the title: {post}."
-            generate_summary(title=post, prompt=prompt, directory='history_video_scripts')
+            generate_summary(title=post, prompt=prompt,
+                             directory='history_video_scripts')
             time.sleep(2)
 
     driver.quit()
@@ -140,16 +144,17 @@ def get_history_articles():
 
 def generate_summary(title, prompt, directory):
     # You can use the following keywords to help you: {topic}."
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=450,
-        n=1,
-        stop=None,
-        timeout=10,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a script writer for a YouTube channel that makes one minute videos about random topics. You are given a title of a post from a random subreddit. You must research the post and write a complete and concise one minute YouTube video about the post. In the style of a VSauce video without ever referring to the narrator. Imagine you're explaining to someone who has never heard of it before, but never refer to the post as a post. If it's applicable, show the historical context and the cultural and social impact of the post."},
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
     )
-    summary = response.choices[0].text.strip()
+    summary = response.choices[0].message.content
     # write to a txt file with the title and the date as the filename
     project_dir = Path(__file__).resolve().parents[1]
     filename = f"{directory}/{title} {dt.datetime.now().strftime('%Y-%m-%d')}.txt"
@@ -162,6 +167,6 @@ def generate_summary(title, prompt, directory):
 
 
 if __name__ == "__main__":
-    # get_tech_articles()
+    get_tech_articles()
     # get_programming_examples(programming_languages[0], programming_methods[3])
-    get_history_articles()
+    # get_history_articles()
